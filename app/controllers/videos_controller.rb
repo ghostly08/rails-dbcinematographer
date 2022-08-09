5d6acb44630e9b83d667e0d447134d41bfaef2e8
ghostly08 <steven.produccionbcn@gmail.com>
2022-08-10 01:55:33 +0200

    displaying simple form

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -12,7 +12,7 @@ class VideosController < ApplicationController
     @video = Video.new(video_params)
     @video.save
 
-    redirect_to video_path(@video)
+    redirect_to videos_path(@videos)
   end
 
   private
